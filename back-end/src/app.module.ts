edce52458580e83b49c8fdc7541fba46ac25d2b4
Pedro Henrique <pedro.o.silva.henrique@gmail.com>
2024-12-15 01:34:13 -0300

    chore: add migration chore: add dockerfile chore: add healthcheck

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [UsersModule, PrismaModule],
+  controllers: [AppController],
 })
 export class AppModule {}
